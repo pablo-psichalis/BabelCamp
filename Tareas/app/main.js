b8d25ebacbe6a2c980ad9330b50053d095ea38d5
Pablo Psichalis Ruiz <pablo.psrz@gmail.com>
2018-02-23 13:45:12 +0100

    Manejo de LocalStorage

--- Tareas/app/main.js
+++ Tareas/app/main.js
@@ -2,11 +2,21 @@ class Main {
     
     constructor(){
         this.user = {
-            nombre: ''
+            nombre: localStorage.getItem('nombre')
         }
 
         this.tarea = ""
-        this.aTareas = []
+        
+        // Si se consigue el item tareas del localStorage,
+        // el array será el item tareas
+        // en caso contrario, será definido como un array vacío
+        // Esto es necesario para especificar que aTareas es un array
+        // y que no de problemas más tarde
+        this.aTareas = 
+            JSON.parse(localStorage.getItem("tareas"))? 
+                JSON.parse(localStorage.getItem("tareas"))
+                 : []
+
         this.vista = {
             inNombre: document.querySelector('#nombre'),
             inTarea: document.querySelector('#tarea'),
@@ -21,27 +31,42 @@ class Main {
 
         this.vista.btnAdd.addEventListener('click',
             this.btnAdd.bind(this), false)
+
     }
     
     btnRegistrar(){
         this.user.nombre = this.vista.inNombre.value
-        console.log(this.user.nombre)
-        this.vista.resultado.innerHTML = `<p>Hola ${this.user.nombre}</p>`
-        this.vista.resultado.classList.add("rojo")
+        localStorage.setItem("nombre", this.user.nombre)
+        this._mostrarNombre()
+    }
+
+    _mostrarNombre(){
+        if(this.user.nombre){
+            console.log(this.user.nombre)
+            this.vista.resultado.innerHTML = `<p>Hola ${this.user.nombre}</p>`
+            this.vista.resultado.classList.add("rojo")
+        }
     }
 
     btnAdd(){
-        let lista
         this.tarea = this.vista.inTarea.value
         this.aTareas.push(this.tarea)
-        
-        lista = "<ul>"
-        this.aTareas.forEach(element => {
-            lista += `<li>${element}</li>`
-        });
-        lista += "</ul>"
 
-        this.vista.tareas.innerHTML = lista
+        localStorage.setItem("tareas", JSON.stringify(this.aTareas))
+        this._mostrarTareas()
+    }
+
+    _mostrarTareas() {
+        let lista
+        if(this.aTareas.length){
+            lista = "<ul>"
+            this.aTareas.forEach(element => {
+                lista += `<li>${element}</li>`
+            });
+            lista += "</ul>"
+
+            this.vista.tareas.innerHTML = lista
+        }
     }
     
 }

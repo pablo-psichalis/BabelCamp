6fccc84326e12b279843818f06aa3b867b34dfc7
Pablo Psichalis Ruiz <pablo.psrz@gmail.com>
2018-03-06 11:55:19 +0100

    Pruebas con modulos y componentes

--- base-app/src/app/app.module.ts
+++ base-app/src/app/app.module.ts
@@ -4,17 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { CabezaComponent } from './cabeza/cabeza.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabezaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+// Está vacío a posta porque un módulo se trata de un
+// contenedor de referencias a los distintos
+// elementos que forman parte de un módulo
+// (declaraciones, importaciones, providers, exportaciones, etc)
+//
+// Main -> Modulo inicial -> bootstrap[AppComponent] (componente inicial) ...
+//
 export class AppModule { }
